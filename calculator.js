46c34690010cb194b799217475433d86d7534738
Rituraj-Ranjan <88331229+Rituraj-Ranjan@users.noreply.github.com>
2021-08-02 19:38:44 +0530

    Add files via upload
    Calculator

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function add(var1,var2){
+    var sum= Number(var1)+Number(var2)
+    alert('Sum is '+sum)
+}
+function sub(var1,var2){
+    var sum= Number(var1)-Number(var2)
+    alert('Sum is '+sum)
+}
+function mul(var1,var2){
+    var sum= Number(var1)*Number(var2)
+    alert('Sum is '+sum)
+}
+function div(var1,var2){
+    var sum= Number(var1)/Number(var2)
+    alert('Sum is '+sum)
+}
